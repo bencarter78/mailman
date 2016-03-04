e0376e8245c3dfe67b5a44e45c234f48e72d45a6
Ben Carter <ben.carter@totalpeople.co.uk>
2016-03-04 15:13:28 +0000

    Fixing issue

--- src/TCK/Mailman/Mailman.php
+++ src/TCK/Mailman/Mailman.php
@@ -1,131 +1,122 @@
-<?php namespace TCK\Mailman;
+<?php
+namespace TCK\Mailman;
 
 use Config, Mail;
-use Illuminate\Mail\Mailer;
+use Illuminate\Contracts\Mail\Mailer;
 
-abstract class Mailman {
+abstract class Mailman
+{
+    /**
+     * @var Mailer
+     */
+    protected $mailer;
 
-	/**
-	 * @var Mailer
-	 */
-	protected $mailer;
+    /**
+     * @param Mailer $mailer
+     */
+    public function __construct(Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
 
-	/**
-	 * @param Mailer $mailer
-	 */
-	public function __construct( Mailer $mailer )
-	{
-		$this->mailer = $mailer;
-	}
+    /**
+     * Sends Email
+     *
+     * @param    int    $user
+     * @param    string $view
+     * @param    string $subject
+     * @param    array  $data
+     * @return    bool
+     */
+    public function sendTo($user, $subject, $view, $data = [])
+    {
+        $recipient = is_object($user) ? $user->email : $user;
 
-	/**
-	 * Sends Email
-	 *
-	 * @param    int    $user
-	 * @param    string $view
-	 * @param    string $subject
-	 * @param    array  $data
-	 * @return    bool
-	 */
-	public function sendTo( $user, $subject, $view, $data = [ ] )
-	{
-		$recipient = is_object( $user ) ? $user->email : $user;
+        $data['user'] = $user;
 
-		$data['user'] = $user;
+        try {
+            // Queue it up and send it!
+            $this->mailer->send($view, $data, function ($m) use ($recipient, $subject, $data) {
+                $this->overwriteFrom($m, $data);
+                $this->addAttachment($m, $data);
+                $this->setCC($m, $data);
+                $this->setBC($m, $data);
 
-		try
-		{
-			// Queue it up and send it!
-			$this->mailer->send( $view, $data, function ( $m ) use ( $recipient, $subject, $data )
-			{
-				$this->overwriteFrom( $m, $data );
-				$this->addAttachment( $m, $data );
-				$this->setCC( $m, $data );
-				$this->setBC( $m, $data );
+                return $m->to($recipient)->subject($subject);
+            });
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
 
-				return $m->to( $recipient )->subject( $subject );
-			} );
-		}
-		catch ( Exception $e )
-		{
-			return $e->getMessage();
-		}
-	}
+    /**
+     * @param $m
+     * @param $data
+     */
+    function overwriteFrom($m, $data)
+    {
+        if (isset( $data['overwriteFrom'] )) {
+            $m->from($data['email'], $this->getFromName($data));
+        }
+    }
 
-	/**
-	 * @param $m
-	 * @param $data
-	 */
-	function overwriteFrom( $m, $data )
-	{
-		if ( isset( $data['overwriteFrom'] ) )
-		{
-			$m->from( $data['email'], $this->getFromName( $data ) );
-		}
-	}
+    /**
+     * @param      $data
+     * @param null $fname
+     * @param null $surname
+     * @return string
+     */
+    public function getFromName($data, $fname = null, $surname = null)
+    {
+        if (isset( $data['first_name'] )) {
+            $fname = $data['first_name'] . ' ';
+        }
 
-	/**
-	 * @param      $data
-	 * @param null $fname
-	 * @param null $surname
-	 * @return string
-	 */
-	public function getFromName( $data, $fname = null, $surname = null )
-	{
-		if ( isset( $data['first_name'] ) )
-		{
-			$fname = $data['first_name'] . ' ';
-		}
+        if (isset( $data['surname'] )) {
+            $surname = $data['surname'];
+        }
 
-		if ( isset( $data['surname'] ) )
-		{
-			$surname = $data['surname'];
-		}
+        return $fname . $surname;
+    }
 
-		return $fname . $surname;
-	}
+    /**
+     * @param $m
+     * @param $data
+     */
+    function addAttachment($m, $data)
+    {
+        if (isset( $data['attachment'] )) {
+            return $m->attach($data['attachment'], ['mime' => 'application/pdf']);
+        }
+    }
 
-	/**
-	 * @param $m
-	 * @param $data
-	 */
-	function addAttachment( $m, $data )
-	{
-		if ( isset( $data['attachment'] ) )
-		{
-			return $m->attach( $data['attachment'], [ 'mime' => 'application/pdf' ] );
-		}
-	}
+    /**
+     * @param $m
+     * @param $data
+     * @return mixed
+     */
+    public function setCC($m, $data)
+    {
+        if (isset( $data['cc'] )) {
+            $m->cc($data['cc']);
 
-	/**
-	 * @param $m
-	 * @param $data
-	 * @return mixed
-	 */
-	public function setCC( $m, $data )
-	{
-		if ( isset( $data['cc'] ) )
-		{
-			$m->cc = $data['cc'];
+            return $m;
+        }
+    }
 
-			return $m;
-		}
-	}
+    /**
+     * @param $m
+     * @param $data
+     * @return mixed
+     */
+    public function setBC($m, $data)
+    {
+        if (isset( $data['bc'] )) {
+            $m->cc($data['bc']);
 
-	/**
-	 * @param $m
-	 * @param $data
-	 * @return mixed
-	 */
-	public function setBC( $m, $data )
-	{
-		if ( isset( $data['bc'] ) )
-		{
-			$m->cc = $data['bc'];
-
-			return $m;
-		}
-	}
+            return $m;
+        }
+    }
 
 
 }
